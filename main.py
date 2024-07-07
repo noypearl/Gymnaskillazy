8bf2801b8a3929c6fe0eca3b749785bf23447082
noypearl <11259340+noypearl@users.noreply.github.com>
2024-07-07 13:42:55 +0300

    supporting offline polling mode

--- main.py
+++ main.py
@@ -1,18 +1,54 @@
 import os
-
+import json
+import asyncio
+import os
+import requests
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, CallbackContext
 from bot import TelegramBot
 from dotenv import load_dotenv
 from constants import CHATGPT_PROMPT
 
-if __name__ == '__main__':
-    load_dotenv()
+load_dotenv()
 
-    TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
-    NOTION_TOKEN = os.getenv('NOTION_TOKEN')
-    NOTION_DATABASE_ID = os.getenv('NOTION_DATABASE_ID')
-    GOOGLE_SHEETS_CREDENTIALS_FILE = 'credentials.json'
-    GOOGLE_SHEETS_ID = os.getenv('GOOGLE_SHEETS_ID')
-    OPENAI_API_TOKEN = os.getenv('OPENAI_API_TOKEN')
-    bot = TelegramBot(TELEGRAM_TOKEN, NOTION_TOKEN, NOTION_DATABASE_ID, GOOGLE_SHEETS_CREDENTIALS_FILE,
-                      GOOGLE_SHEETS_ID, OPENAI_API_TOKEN)
-    bot.application.run_polling()
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+NOTION_TOKEN = os.getenv('NOTION_TOKEN')
+NOTION_DATABASE_ID = os.getenv('NOTION_DATABASE_ID')
+GOOGLE_SHEETS_CREDENTIALS_FILE = 'credentials.json'
+GOOGLE_SHEETS_ID = os.getenv('GOOGLE_SHEETS_ID')
+OPENAI_API_TOKEN = os.getenv('OPENAI_API_TOKEN')
+WEBHOOK_URL = os.getenv('WEBHOOK_URL')
+SECRET_TOKEN = os.getenv('SECRET_TOKEN')
+
+bot = TelegramBot(TELEGRAM_TOKEN, NOTION_TOKEN, NOTION_DATABASE_ID, GOOGLE_SHEETS_CREDENTIALS_FILE,
+                  GOOGLE_SHEETS_ID, OPENAI_API_TOKEN, WEBHOOK_URL, SECRET_TOKEN)
+application = bot.application
+
+
+async def process_update(event):
+    # update = event
+    # event = e
+    update = Update.de_json(event, application.bot)
+    await bot.application.initialize()
+    await bot.application.process_update(update)
+
+
+def main(event=None):
+    if event is None:
+        # Polling mode
+        print("No event - running the app in Telegram Polling mode (no "
+              "webhook)")
+        application.run_polling()
+    else:
+        # Webhook mode
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(process_update(event))
+        return {
+            'statusCode': 200,
+            'body': json.dumps('Success')
+        }
+
+
+
+if __name__ == '__main__':
+    main()
